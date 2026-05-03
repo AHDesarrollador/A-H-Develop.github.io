e69ed524ebc07fd82d2b94e5eaa0ff8cf8298032
Ariel <ariel.hc8@outlook.com>
2026-05-03 14:45:39 -0600

    Pagina desplegadav1.0

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/A-H-Develop.github.io/',  // ← reemplaza con el nombre exacto del repo
+  base: '/',
 });
